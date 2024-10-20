4ad61bc59141d6e4ad67e14cba619e645db1ded0
Ricardo <ricardobatista893@gmail.com>
2024-10-20 16:37:30 -0300

    findByEndDate etc

--- src/main/java/com/fds/siscaa/interfaceAdapters/repository/jpa/SubscriptionRepositoryJPA.java
+++ src/main/java/com/fds/siscaa/interfaceAdapters/repository/jpa/SubscriptionRepositoryJPA.java
@@ -1,5 +1,6 @@
 package com.fds.siscaa.interfaceAdapters.repository.jpa;
 
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 
@@ -11,14 +12,21 @@ import com.fds.siscaa.interfaceAdapters.repository.model.SubscriptionModel;
 
 public interface SubscriptionRepositoryJPA extends JpaRepository<SubscriptionModel, Long> {
     List<SubscriptionModel> findAll();
+
     List<SubscriptionModel> findByEndDate(Date endDate);
+
+    List<SubscriptionModel> findByEndDateAfter(LocalDate currentDate);
+
+    List<SubscriptionModel> findByEndDateBefore(LocalDate currentDate);
+
     List<SubscriptionModel> findByClientCode(long clientCode);
+
     List<SubscriptionModel> findByApplicationCode(long applicationCode);
+
     SubscriptionModel findByCode(long code);
 
     @Modifying()
     @Query("update SubscriptionModel s set s.startDate = startDate, s.endDate = endDate where s.code = subscriptionCode")
     int updateSubscriptionStartDateAndEndDateByCode(Date startDate, Date endDate, long subscriptionCode);
-
 
 }
