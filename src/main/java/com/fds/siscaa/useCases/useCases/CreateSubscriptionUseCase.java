2000823cc3131febf9799a68e3782a188977b33d
GabiHert <hertergabriel4@gmail.com>
2024-10-21 18:46:36 -0300

    refactor professor

--- src/main/java/com/fds/siscaa/useCases/useCases/CreateSubscriptionUseCase.java
+++ src/main/java/com/fds/siscaa/useCases/useCases/CreateSubscriptionUseCase.java
@@ -1,29 +1,20 @@
 package com.fds.siscaa.useCases.useCases;
 
 import java.time.LocalDate;
-import java.time.ZoneId;
-import java.util.Date;
 
 import com.fds.siscaa.domain.entity.SubscriptionEntity;
-import com.fds.siscaa.useCases.adapters.ApplicationRepositoryAdapter;
-import com.fds.siscaa.useCases.adapters.ClientRepositoryAdapter;
 import com.fds.siscaa.useCases.adapters.SubscriptionRepositoryAdapter;
 
 import lombok.AllArgsConstructor;
 
-import com.fds.siscaa.domain.entity.ApplicationEntity;
-import com.fds.siscaa.domain.entity.ClientEntity;
-
 @AllArgsConstructor()
 public class CreateSubscriptionUseCase {
-
-    private final ClientRepositoryAdapter clientRepository;
-    private final ApplicationRepositoryAdapter applicationRepository;
+    private final int daysToExtend = 7;
     private final SubscriptionRepositoryAdapter subscriptionRepository;
 
     public SubscriptionEntity create(long clientCode, long applicationCode) {
         LocalDate startDate = LocalDate.now();
-        LocalDate endDate = startDate.plusDays(7);
+        LocalDate endDate = startDate.plusDays(daysToExtend);
 
         SubscriptionEntity subscriptionEntity = new SubscriptionEntity(
                 clientCode,
